721c0ea65271f1ebb07f805a3c0be18aa626df95
Mateo2131 <goz.matu@gmail.com>
2023-02-27 12:51:40 -0300

    feat: add Logo & favicon

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -1,11 +1,12 @@
 <script>
 	import BoardMenu from '#components/BoardMenu.svelte';
 	import BoardSettings from '#components/BoardSettings.svelte';
+	import Logo from './Logo.svelte';
 </script>
 
 <header class="h-16 w-full fixed top-0 z-50 border-b border-zinc-700">
-	<div class="h-full flex items-center justify-between px-3 max-w-screen-xl mx-auto">
-		<a href="/" class="text-xl font-medium">Kanbi</a>
+	<div class="h-full flex items-center justify-between gap-2 px-3 max-w-screen-xl mx-auto">
+		<Logo />
 
 		<div class="flex items-center gap-2">
 			<BoardMenu />
---
+++ web/src/lib/components/Logo.svelte
@@ -0,0 +1,3 @@
+<a href="/">
+	<span class="text-[#8789FE] uppercase text-2xl font-medium tracking-widest">Kanbi</span>
+</a>
